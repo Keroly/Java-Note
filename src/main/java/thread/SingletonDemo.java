32ced215e4054d2b177921c60e8c74b3168f31b3
Keroly <15122112779@163.com>
2021-03-23 08:35:26 +0800

    单例模式的安全性问题

---
+++ src/main/java/thread/SingletonDemo.java
@@ -0,0 +1,47 @@
+package thread;
+
+public class SingletonDemo {
+
+    // 饿汉式
+    // private static SingletonDemo instance = new SingletonDemo();
+    private static volatile SingletonDemo instance = null; // 双重校验
+
+    private SingletonDemo() {
+        System.out.println(Thread.currentThread().getName() + "\t 构造方法");
+    }
+
+/*
+    // 懒汉式
+    public static SingletonDemo getInstance() {
+        if (instance == null) {
+            instance = new SingletonDemo();
+        }
+        return instance;
+    }
+*/
+    // DCL (Double Check lOCK) 双重校验锁
+    public static SingletonDemo getInstance() {
+        if (instance == null) {
+            synchronized (SingletonDemo.class) {
+                if (instance == null) {
+                    instance = new SingletonDemo();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+/*        // 单线程
+        System.out.println(SingletonDemo.getInstance() == SingletonDemo.getInstance());
+        System.out.println(SingletonDemo.getInstance() == SingletonDemo.getInstance());
+        System.out.println(SingletonDemo.getInstance() == SingletonDemo.getInstance());
+ */
+        // 多线程
+        for (int i = 0; i < 1000; i++) {
+            new Thread(() -> {
+                SingletonDemo.getInstance();
+            }, String.valueOf(i)).start();
+        }
+    }
+}
