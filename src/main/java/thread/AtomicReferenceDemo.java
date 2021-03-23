5c34f03dcea14495eaed31c68c48ebe0a016196e
Keroly <15122112779@163.com>
2021-03-23 14:25:03 +0800

    原子引用

---
+++ src/main/java/thread/AtomicReferenceDemo.java
@@ -0,0 +1,28 @@
+package thread;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+class User {
+    String username;
+    int age;
+    public User(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+}
+
+// 理解原子引用
+public class AtomicReferenceDemo {
+    public static void main(String[] args) {
+        User z3 = new User("z3", 23);
+        User l4 = new User("l4", 24);
+
+        AtomicReference<User> atomicReference = new AtomicReference<>();
+        atomicReference.set(z3);
+
+        System.out.println(atomicReference.compareAndSet(z3, l4) + "\t" + atomicReference.get().age);
+        System.out.println(atomicReference.compareAndSet(z3, l4) + "\t" + atomicReference.get().age);
+
+    }
+}
