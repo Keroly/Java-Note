34ef414dc5cef5e40e59bd6713d24429ec378af1
Keroly <15122112779@163.com>
2021-03-24 10:36:58 +0800

    Semaphore

---
+++ src/main/java/lock/SemaphoreDemo.java
@@ -0,0 +1,24 @@
+package lock;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class SemaphoreDemo {
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(3); // 模拟三个停车位
+        for (int i = 0; i < 6; i++) { // 6部车
+            new Thread(() -> {
+                try {
+                    semaphore.acquire();
+                    System.out.println(Thread.currentThread().getName() + "\t 抢到");
+                    TimeUnit.SECONDS.sleep(3);
+                    System.out.println(Thread.currentThread().getName() + "\t 离开");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    semaphore.release();
+                }
+            }, String.valueOf(i)).start();
+        }
+    }
+}
