b6553348dfb14ad0ed0ffdc97dcae2ec98161089
Keroly <15122112779@163.com>
2021-03-25 18:52:54 +0800

    引用计数器法

---
+++ src/main/java/jvm/RefCountGC.java
@@ -0,0 +1,17 @@
+package jvm;
+
+public class RefCountGC {
+    private byte[] bigsize = new byte[2 * 1024 * 1024]; // 这个属性是为了占用内存
+    Object instance = null;
+
+    public static void main(String[] args) {
+        RefCountGC object1= new RefCountGC();
+        RefCountGC object2= new RefCountGC();
+        object1.instance = object2;
+        object2.instance = object1;
+        object1 = null;
+        object2 = null;
+
+        System.gc();    // 不是立刻启动
+    }
+}
